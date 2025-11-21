79f10fd4c2321af536d6074ede30966225e70936
tristan_retali <tristan.retali@gmail.com>
2025-11-21 13:36:20 +0100

    Ajout de toute les routes de lecture

--- app/crud.py
+++ app/crud.py
@@ -1,11 +1,46 @@
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from typing import List
 from models import Department, Indicateur
 from schemas import DepartmentCreate, DepartmentRead, IndicateurCreate, IndicateurRead
 from database import get_db
 
 
-def get_departments(session: Session, skip: int = 0, limit: int = 100):
-    departments = session.query(Department).offset(skip).limit(limit).all()
+def get_departments(session: Session, skip: int = 0, limit: int = 10):
+    departments = (
+        session.query(Department)
+        .options(joinedload(Department.indicateurs))
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
     return departments
+
+
+def get_department(id: int, session: Session):
+    department = (
+        session.query(Department).options(joinedload(Department.indicateurs)).get(id)
+    )
+    if not department:
+        raise HTTPException(status_code=404, detail="Département non trouvé")
+    return department
+
+
+def get_indicateurs(session: Session, skip: int = 0, limit: int = 10):
+    indicateurs = (
+        session.query(Indicateur)
+        .options(joinedload(Indicateur.department))
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+    return indicateurs
+
+
+def get_indicateur(id: int, session: Session):
+    indicateur = (
+        session.query(Indicateur).options(joinedload(Indicateur.department)).get(id)
+    )
+    if not indicateur:
+        raise HTTPException(status_code=404, detail="Indicateur non trouvé")
+    return indicateur
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from crud import get_departments
+from crud import get_departments, get_department, get_indicateurs, get_indicateur
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
@@ -8,6 +8,28 @@ router = APIRouter()
 
 
 @router.get("/departments/")
-def read_departments(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_departments(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     departments = get_departments(db, skip=skip, limit=limit)
+
     return departments
+
+
+@router.get("/department/{id}")
+def read_department(id: int, db: Session = Depends(get_db)):
+    department = get_department(id, db)
+
+    return department
+
+
+@router.get("/indicateurs/")
+def read_indicateurs(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    indicateurs = get_indicateurs(db, skip=skip, limit=limit)
+
+    return indicateurs
+
+
+@router.get("/indicateur/{id}")
+def read_indicateur(id: int, db: Session = Depends(get_db)):
+    indicateur = get_indicateur(id, db)
+
+    return indicateur
